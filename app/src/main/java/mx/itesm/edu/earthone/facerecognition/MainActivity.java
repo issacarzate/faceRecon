b6fd68b562e5c9c89f2101457f8dea13a3813a3f
earthone <earthone@earthones-MacBook-Pro.local>
2018-03-13 11:21:56 -0600

    Initial commit

---
+++ app/src/main/java/mx/itesm/edu/earthone/facerecognition/MainActivity.java
@@ -0,0 +1,134 @@
+package mx.itesm.edu.earthone.facerecognition;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.face.FaceDetector;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "FACE API";
+    private static final int PHOTO_REQUEST = 10;
+    private TextView textView;
+    private ImageView imageView;
+    private Uri imageUri;
+    private FaceDetector detector;
+    private static final int REQUEST_WRITE_PERMISSION = 20;
+    private static final String SAVED_INSTANCE_URI = "uri";
+    private static final String SAVED_INSTANCE_BITMAP = "bitmap";
+    private static final String SAVED_INSTANCE_RESULT = "result";
+    Bitmap editedBitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.results);
+        imageView = (ImageView) findViewById(R.id.scannedResults);
+        if (savedInstanceState != null) {
+            editedBitmap = savedInstanceState.getParcelable(SAVED_INSTANCE_BITMAP);
+            if (savedInstanceState.getString(SAVED_INSTANCE_URI) != null) {
+                imageUri = Uri.parse(savedInstanceState.getString(SAVED_INSTANCE_URI));
+            }
+            imageView.setImageBitmap(editedBitmap);
+            textView.setText(savedInstanceState.getString(SAVED_INSTANCE_RESULT));
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_WRITE_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    picTime();
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PHOTO_REQUEST && resultCode == RESULT_OK) {
+            launchCamera();
+
+            try {
+
+
+            } catch (Exception e) {
+                Toast.makeText(this, "Failed to load Image", Toast.LENGTH_SHORT).show();
+                Log.e(LOG_TAG, e.toString());
+            }
+        }
+    }
+
+
+    private void picTime() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = new File(Environment.getExternalStorageDirectory(), "picture.jpg");
+        imageUri = FileProvider.getUriForFile(MainActivity.this,
+                BuildConfig.APPLICATION_ID + ".provider", photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, PHOTO_REQUEST);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (imageUri != null) {
+            outState.putParcelable(SAVED_INSTANCE_BITMAP, editedBitmap);
+            outState.putString(SAVED_INSTANCE_URI, imageUri.toString());
+            outState.putString(SAVED_INSTANCE_RESULT, textView.getText().toString());
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void launchCamera() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        mediaScanIntent.setData(imageUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private Bitmap decodeBitmapUri(Context ctx, Uri uri) throws FileNotFoundException {
+        int targetW = 600;
+        int targetH = 600;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(ctx.getContentResolver().openInputStream(uri), null, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        return BitmapFactory.decodeStream(ctx.getContentResolver()
+                .openInputStream(uri), null, bmOptions);
+    }
+}
